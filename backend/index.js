579a282592e02fec6cc9c8a02edbc3eb0d3a60b3
MunyinSam <kelvinsam233@gmail.com>
2025-08-07 10:52:25 +0700

    index.js init

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const PORT = 15000
+
+const mongoURL = process.env.MONGO_URL || ''
+
+app.use(cors())
+app.use(express.json())
+
+const Task = mongoose.model(
+    'Task',
+    new mongoose.Schema({
+        text: String,
+        completed: Boolean,
+    })
+)
+
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
+})
+
+app.post('/tasks', async (req, res) => {
+    const task = await Task.create(req.body)
+    res.json(task)
+})
+
+app.put('tasks/:id', async (req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body)
+    res.json(task)
+})
+
+app.delete('delete/:id', async (req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    res.json(task)
+})
+
+const connectWithRetry = () => {
+    console.log('Trying to connect to MongoDB...')
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log('MongoDB Connected')
+            app.listen(PORT, () => {
+                console.log(`Backend running on port ${PORT}`)
+            })
+        })
+        .catch((err) => {
+            console.error(
+                'MongoDB connection error. Retrying in 5s...',
+                err.message
+            )
+            setTimeout(connectWithRetryl, 5000)
+        })
+}
+
+connectWithRetry()
